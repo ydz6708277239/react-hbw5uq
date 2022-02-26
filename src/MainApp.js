d92ea65d3106443fb1419cbffc4378b418fe9250
wuda20 <daniel.wu35@qmail.cuny.edu>
2022-02-26 17:44:36 -0500

    2 files modified

--- src/MainApp.js
+++ src/MainApp.js
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import Ticker from 'react-ticker';
+
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
